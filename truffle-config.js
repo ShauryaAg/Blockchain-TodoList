0390ae033bc5abee2aeaf5843442a5de0d94cee4
ShauryaAg <agarwal.lucky.shaurya11@gmail.com>
2020-04-21 20:26:05 +0530

    Done

---
+++ truffle-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+    },
+
+  },
+  // Configure your compilers
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      },
+    }
+  }
+}
